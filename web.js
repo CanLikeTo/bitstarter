0b0b84a8d6c4bbb736e9484adc4b9e5218961bee
Rael Schmulian <rael@ubuntu-r.(none)>
2013-07-11 10:45:29 +0200

    Added index.html and modified web.js to show its content

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+function getFileContent(filename) {
+  buff = fs.readFileSync(filename);
+  return buff.toString();
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(getFileContent('index.html'));
 });
 
 var port = process.env.PORT || 5000;
